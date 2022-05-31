a082398d36433a0eb34afd2b4cb2846bf822caa9
Ben Leung <benjamin.leung80@gmail.com>
2022-05-31 15:13:07 +0800

    Update OSSDstemCDNIS2022.py

--- OSSDstemCDNIS2022.py
+++ OSSDstemCDNIS2022.py
@@ -2,21 +2,6 @@ from microbit import *
 
 
 
-# show light if
-def showLights():
-    print('--------------')
-    print('show lights')
-    print('--------------')
-    for x in range(5):
-        for y in range(5):
-            if matrix[x][y] == 1:
-                display.set_pixel(x,y,9)
-            else:
-                display.set_pixel(x,y,0)
-        print()
-
-
-
 # Main code -----------------------------------------------
 
 matrix = [ [0]*5 for i in range(5)]
@@ -25,17 +10,18 @@ matrix = [ [0]*5 for i in range(5)]
 
 while True:
 
-    readLight = pin0.read_analog() # read analog pin
+    readLight = pin0.read_analog() # read analog pin from pin 0
 
 
-    # store any left over values into array by adding 1
-#    t = readLight / 40 # divide analog pin value by 40 as max value is 1023
+
     t = readLight - 830 # divide analog pin value by 40 as max value is 1023
     t = t / 4
+    # the goal is to get the variable t to have a value of about 25 during regular room light
+    # this way all the lights should be on in the following double loop
+    # when resistance to the LDR increases the value should drop to 0
 
 
-
-
+    # go through matrix and show light for the value of t
     for x in range(5):
         for y in range(5):
             if t > 1:
@@ -43,7 +29,3 @@ while True:
             else:
                 display.set_pixel(x,y,0)
             t = t - 1
-
-
-
-#    showLights()
