fb97b0fc4fb515b0620dc74fb020b16b3560284b
Ben Leung <benjamin.leung80@gmail.com>
2022-05-31 15:20:26 +0800

    Update OSSDstemCDNIS2022.py

--- OSSDstemCDNIS2022.py
+++ OSSDstemCDNIS2022.py
@@ -3,7 +3,13 @@ from microbit import *
 
 
 # Main code -----------------------------------------------
+# -------------- explanation of how the code works
+# the goal is to get the variable t to have a value of about 25 during regular room light
+# this way all the lights should be on in the following double loop
+# when resistance to the LDR increases the value should drop to 0
 
+
+# create a 2D array
 matrix = [ [0]*5 for i in range(5)]
 
 
@@ -14,11 +20,10 @@ while True:
 
 
 
-    t = readLight - 830 # divide analog pin value by 40 as max value is 1023
+# ---------------------------------- STUDENT'S SHOULD ONLY MODIFY CODE HERE ----------------------------------
+    t = readLight - 790 # divide analog pin value by 40 as max value is 1023
     t = t / 4
-    # the goal is to get the variable t to have a value of about 25 during regular room light
-    # this way all the lights should be on in the following double loop
-    # when resistance to the LDR increases the value should drop to 0
+# ---------------------------------- STUDENT'S SHOULD ONLY MODIFY CODE HERE ----------------------------------
 
 
     # go through matrix and show light for the value of t
@@ -29,3 +34,4 @@ while True:
             else:
                 display.set_pixel(x,y,0)
             t = t - 1
+
